41538c7b1618abdb9aecaa6d89fd7d95c4c12c81
Michał 'Griwes' Dominiak <griwes@griwes.info>
2014-06-02 23:40:29 +0200

    Moar features.

--- filter.h
+++ filter.h
@@ -18,11 +18,13 @@ namespace
     }
 }
 
+void pay_filter_cost();
+
 class filter
 {
 public:
-    filter(const std::uintmax_t & timestamp, std::uintmax_t & dirt, std::priority_queue<event, std::vector<event>, std::greater<event>> & events) : _timestamp{ timestamp }, 
-        _dirt{ dirt }, _events(events)
+    filter(const std::uintmax_t & timestamp, std::uintmax_t & dirt, std::priority_queue<event, std::vector<event>, std::greater<event>> & events,
+        std::uintmax_t & repairman) : _timestamp{ timestamp }, _dirt{ dirt }, _events(events), _repairman{ repairman }
     {
     }
     
@@ -45,13 +47,29 @@ public:
             std::cout << "At timestamp " << _timestamp << " a filter has failed\n";
             
             static std::normal_distribution<> dist(120, 60);
+            auto repair_time = dist(engine);
+            if (_repairman > _timestamp)
+            {
+                repair_time += _repairman;
+            }
             
-            _events.emplace(_timestamp + dist(engine), [this](std::uintmax_t){ repair(); });
+            else
+            {
+                repair_time += _timestamp;
+            }
+            
+            if (_timestamp >= _fault) 
+            { 
+                pay_filter_cost(); 
+            } 
+            
+            _events.emplace(repair_time, [this](std::uintmax_t){ repair(); });
+            _repairman = repair_time;
             
             return;
         }
         
-        static std::normal_distribution<> dist(100, 25);
+        static std::normal_distribution<> dist(50, 25);
         
         std::uintmax_t dirt_filtered = std::min<std::intmax_t>(dist(engine), _dirt);
         _dirt -= dirt_filtered;
@@ -75,4 +93,5 @@ private:
     std::uintmax_t _filter_dirt = 0;
     std::priority_queue<event, std::vector<event>, std::greater<event>> & _events;
     std::uintmax_t _fault = 0;
+    std::uintmax_t & _repairman;
 };
--- main.cpp
+++ main.cpp
@@ -1,28 +1,43 @@
 #include <queue>
 #include <functional>
 #include <random>
+#include <fstream>
 
 #include "event.h"
 #include "filter.h"
 
-constexpr std::uintmax_t timestep = 1;
-constexpr std::uintmax_t max_timestamp = 24 * 365;
-constexpr std::uintmax_t num_filters = 5;
+constexpr std::uintmax_t timestep = 1;            // half an hour
+std::uintmax_t max_timestamp = 2 * 24 * 365;      // a year
+std::uintmax_t num_filters = 4;                   // five filters
 
 std::random_device rd;
 std::mt19937 engine{ rd() };
 
 std::uintmax_t linear_of_people(std::uintmax_t num_people)
 {
-    return num_people * 3;
+    return num_people * 5;
 }
 
-std::intmax_t normal_people_distribution(std::uintmax_t, std::uintmax_t amount_of_people)
+std::uintmax_t pool_volume = 25 * 50 * 2;
+std::uintmax_t people_limit = 300;
+std::uintmax_t ticket_price = 10;
+
+std::uintmax_t open_at = 6;
+std::uintmax_t close_at = 22;
+
+std::intmax_t normal_people_distribution(std::uintmax_t timestamp, std::uintmax_t amount_of_people)
 {
-    static std::normal_distribution<> dist(0, 5);
+    timestamp /= 2;
+    
+    if ((timestamp % 24) < open_at || (timestamp % 24) > close_at)
+    {
+        return -amount_of_people;
+    }
+    
+    static std::normal_distribution<> dist(0, 20);
     
     auto change = std::round(dist(engine));
-    if (amount_of_people >= 200 && change > 0)
+    if (amount_of_people >= people_limit && change > 0)
     {
         change = 0;
     }
@@ -30,18 +45,43 @@ std::intmax_t normal_people_distribution(std::uintmax_t, std::uintmax_t amount_o
     return std::max<std::intmax_t>(change, -amount_of_people);
 }
 
-int main()
+std::uintmax_t repairman_salary = 50;                       // per half an hour
+std::uintmax_t filter_cost = 15000;                         // let's make a guess
+std::intmax_t money = -filter_cost * num_filters;
+
+void pay_filter_cost()
 {
+    money -= filter_cost;
+}
+
+// either no arguments or: max_timestamp num_filters pool_volume people_limit ticket_price repairman_salary filter_cost open_at close_at
+int main(int argc, char ** argv)
+{
+    if (argc == 10)
+    {
+        max_timestamp = std::stoll(argv[1]);
+        num_filters = std::stoll(argv[2]);
+        pool_volume = std::stoll(argv[3]);
+        people_limit = std::stoll(argv[4]);
+        ticket_price = std::stoll(argv[5]);
+        repairman_salary = std::stoll(argv[6]);
+        filter_cost = std::stoll(argv[7]);
+        open_at = std::stoll(argv[8]);
+        close_at = std::stoll(argv[9]);
+    }
+    
+    std::fstream log{ "basen.csv", std::ios::binary | std::ios::out };
+    
     std::priority_queue<event, std::vector<event>, std::greater<event>> events;
     std::uintmax_t timestamp = 0;
+    std::uintmax_t repairman_available_at = 0;
         
     std::uintmax_t water_dirt_level = 0;
     std::uintmax_t amount_of_people = 0;
     
     std::uintmax_t alarming_level_timestamps = 0;
-    std::uintmax_t people_x_timestamps = 0;
 
-    std::vector<filter> filters(num_filters, filter{ timestamp, water_dirt_level, events });
+    std::vector<filter> filters(num_filters, filter{ timestamp, water_dirt_level, events, repairman_available_at });
     
     std::function<std::uintmax_t(std::uintmax_t)> dirt_function = linear_of_people;
     std::function<std::uintmax_t(std::uintmax_t, std::uintmax_t)> people_function = normal_people_distribution;
@@ -57,6 +97,7 @@ int main()
         }
         
         std::cout << "At timestamp " << timestamp << ": people = " << amount_of_people << ", dirt = " << water_dirt_level << '\n';
+        log << timestamp << ',' << amount_of_people << ',' << water_dirt_level << '\n';
         
         while (events.size() && events.top().timestamp() <= timestamp)
         {
@@ -67,14 +108,22 @@ int main()
         }
         
         timestamp += timestep;
-        people_x_timestamps += amount_of_people;
+        money += amount_of_people;
         
-        if (water_dirt_level >= 10000)
+        if (water_dirt_level / pool_volume >= 5)
         {
             ++alarming_level_timestamps;
+        }
+        
+        if (repairman_available_at > timestamp)
+        {
+            money -= repairman_salary;
         }
     }
     
     std::cout << "Alarming level timestamp count: " << alarming_level_timestamps << '\n';
-    std::cout << "Income: " << people_x_timestamps << '\n';
+    std::cout << "Money: " << money << '\n';
+    
+    log << '\n';
+    log << alarming_level_timestamps << ',' << money << '\n';
 }
